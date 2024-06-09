7f07c30f86c16f0ff6b9a3789d185b0531ac8525
abhisheksavaliya555 <abhisheksavaliya555@gmail.com>
2024-06-09 12:41:16 +0530

    change url message

--- src/app/component/popup/page.js
+++ src/app/component/popup/page.js
@@ -18,13 +18,14 @@ const page = ({ togglePopup }) => {
                 <div className="flex justify-center">
                     <button
                         className="bg-gray-200 rounded-full flex items-center justify-center mr-3"
-                        onClick={() => window.open('https://wa.me/?text=Check%20out%20this%20profile!', '_blank')}
+                        onClick={() => window.open('https://wa.me/?text=👋%20Hey!%20Check%20out%20this%20amazing%20profile:%20https://abhisheksavaliya.vercel.app/%20🎉%20Here%27s%20Abhishek!%20😊', '_blank')}
+
                     >
                         <SocialIcon network="whatsapp" style={{ height: 40, width: 40 }} />
                     </button>
                     <button
                         className="bg-gray-200 rounded-full flex items-center justify-center"
-                        onClick={() => window.open('https://twitter.com/intent/tweet?text=Check%20out%20this%20profile!', '_blank')}
+                        onClick={() => window.open('https://twitter.com/intent/tweet?text=👋%20Hey!%20Check%20out%20this%20amazing%20profile:%20https://abhisheksavaliya.vercel.app/%20🎉%20Here%27s%20Abhishek!%20😊', '_blank')}
                     >
                         <SocialIcon network="x" style={{ height: 40, width: 40 }} />
                     </button>
