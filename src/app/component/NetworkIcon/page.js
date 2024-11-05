ba47dba7c5333e48bbc5284a67abaa11156d997b
abhisheksavaliya555 <abhisheksavaliya555@gmail.com>
2024-11-05 16:27:49 +0530

    update

--- src/app/component/NetworkIcon/page.js
+++ src/app/component/NetworkIcon/page.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { SocialIcon } from 'react-social-icons';
 import profileData from '../../../Data/profile.json';
+import image from '../../../Data/imageShow.json'
 
 const NetworkIcon = ({ network, alt }) => {
-    const imageShow = ['profile', 'paypal', 'shop', 'buymeacoffee'];
+
+    const imageShow = image.imageNetwork;
 
     const networkData = profileData.find((data) => data.network === network);
 
